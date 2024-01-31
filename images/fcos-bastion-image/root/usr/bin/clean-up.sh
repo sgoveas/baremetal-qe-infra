3105cc5e1a75351d807ad72d4b0b8a3c7e993de7
aleskandro <aleskandro@redhat.com>
2024-01-31 14:39:27 +0000

    OCPQE-15774: Fix the cleanup service by exploiting the openshift/release post steps
    Using the post steps defined in openshift/release will allow to
    always execute the most up-to-date post chain and avoid duplicating the
    deprovisioning code.
    This commit introduces:
    - a deprovision.sh script in the prow-image: the post chain in
      openshift/release are not expected to be concurrent safe and executing
      them in the prow-image will ensure a similar environment to the one in
      the post chain pods
    - changes the previous implementation of the prune_nodes script to run a
      container and the deprovision.sh script in the prow-image
    - patch the clean-up.sh script to run based on the /var/builds/*
      content
    - (vendor) enable the systemd timer unit

--- images/fcos-bastion-image/root/usr/bin/clean-up.sh
+++ images/fcos-bastion-image/root/usr/bin/clean-up.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-# Check if any haproxy container is running for more than 3 days
-for i in $(docker ps -a --format "{{.Names}}" | grep "haproxy-")
+echo "<3>Starting the clean up process"
+echo "<2>Checking if any /var/builds/* folder container is older than 3 days and prune the related clusters"
+# Check if any /var/builds/* folder container is older than 3 days
+while IFS= read -r -d '' cluster_folder
 do
-  creation_ts=$(docker inspect --format="{{.Created}}" "$i")
-  create_ts=$(date -d "${creation_ts}" +%s)
-  now_ts=$(date +%s)
-  diff_in_ts=$((now_ts - create_ts))
-  if [ "${diff_in_ts}" -ge 259200 ]; then
-    CLUSTER=${i#haproxy-}
-    echo "<3>$CLUSTER is more than 3 days old, starting the pruning...."
-    prune_nodes "$CLUSTER"
-  fi
-done
+  echo "<3>Deleting $cluster_folder"
+  cluster=$(basename "$cluster_folder")
+  echo "<3>$cluster is more than 3 days old, starting the pruning...."
+  prune_nodes "$cluster"
+done < <(find /var/builds/ -maxdepth 1 -type d -mtime +3)
 
 # Clean up left over ports in the ovs switches that can span across multiple clusters
 for port in $(ovs-vsctl show  | grep "No such device" | sed -e 's/^.*device //' -e 's/ (No such.*$//')
